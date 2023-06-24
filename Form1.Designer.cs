41b7a16faea02b2e558bac16da663bf2fe625380
AhmetSafa0707 <119118562+AhmetSafa0707@users.noreply.github.com>
2023-06-24 16:05:54 +0300

    Combo Box Mantığı
    Ekrandan bir içecek seçiyoruz ve bize, seçtiğimiz içeceği gösteren bir bilgi kutucuğu önümüze geliyor.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Windows_Form_ComboBox_Kullanımı
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.combo_Icecekler = new System.Windows.Forms.ComboBox();
+			this.btn_gunSec = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// combo_Icecekler
+			// 
+			this.combo_Icecekler.BackColor = System.Drawing.Color.Yellow;
+			this.combo_Icecekler.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.combo_Icecekler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.combo_Icecekler.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.combo_Icecekler.FormattingEnabled = true;
+			this.combo_Icecekler.Items.AddRange(new object[] {
+            "Çay",
+            "Kahve",
+            "Milk Shake",
+            "Soğuk Çay",
+            "Su"});
+			this.combo_Icecekler.Location = new System.Drawing.Point(24, 31);
+			this.combo_Icecekler.Name = "combo_Icecekler";
+			this.combo_Icecekler.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.combo_Icecekler.Size = new System.Drawing.Size(368, 48);
+			this.combo_Icecekler.Sorted = true;
+			this.combo_Icecekler.TabIndex = 0;
+			this.combo_Icecekler.Text = "İçecekler";
+			this.combo_Icecekler.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// btn_gunSec
+			// 
+			this.btn_gunSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btn_gunSec.Location = new System.Drawing.Point(24, 117);
+			this.btn_gunSec.Name = "btn_gunSec";
+			this.btn_gunSec.Size = new System.Drawing.Size(368, 76);
+			this.btn_gunSec.TabIndex = 1;
+			this.btn_gunSec.Text = "İçecek Seçiniz";
+			this.btn_gunSec.UseVisualStyleBackColor = true;
+			this.btn_gunSec.Click += new System.EventHandler(this.btn_gunSec_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(777, 437);
+			this.Controls.Add(this.btn_gunSec);
+			this.Controls.Add(this.combo_Icecekler);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox combo_Icecekler;
+		private System.Windows.Forms.Button btn_gunSec;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form_ComboBox_Kullanımı
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{	
+			combo_Icecekler.Items.Add("Gazoz");
+			combo_Icecekler.Items.Add("Maden Suyu");
+
+			combo_Icecekler.Items.RemoveAt(4);// RemoveAt metodu index numarasını baz alır.
+			combo_Icecekler.Items.Remove("Su"); // Remove metodu ise direkt indexi baz alır.
+
+		}
+
+		private void btn_gunSec_Click(object sender, EventArgs e)
+		{
+			string secilenIcecek=combo_Icecekler.SelectedItem.ToString();
+
+			MessageBox.Show("Seçtiğiniz İçecek: " + secilenIcecek);
+		}
+	}
+}
